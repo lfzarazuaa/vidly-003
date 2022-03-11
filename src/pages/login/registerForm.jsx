260fdcbb96b4f2c2849e91d82bc7adefbe4af513
Luis Fernando Zarazua Aguilar <lfzarazuaa1400@outlook.com>
2022-03-11 09:25:10 -0600

    Refactor Code.

--- src/pages/login/registerForm.jsx
+++ src/pages/login/registerForm.jsx
@@ -4,63 +4,63 @@ import Form from "../../components/common/form";
 import { min } from "lodash";
 
 class RegisterForm extends Form {
-	constructor(props) {
-		super(props);
-		this.componentsProps = {
-			username: {
-				name: "username",
-				label: "Username",
-			},
-			password: {
-				name: "password",
-				label: "Password",
-				type: "password",
-			},
-			fullName: {
-				name: "fullname",
-				label: "Full Name",
-			},
-		};
-		const { username, password, fullName } = this.componentsProps;
-		this.schema = {
-			[username.name]: Joi.string()
-				.email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-				.required()
-				.label(username.label),
-			[password.name]: Joi.string()
-				.min(5)
-				.max(20)
-				.alphanum()
-				.required()
-				.label(password.label),
-			[fullName.name]: Joi.string().min(3).required().label(fullName.label),
-		};
-		this.state = {
-			data: {
-				[username.name]: "moshegg",
-				[password.name]: "myS3cretPasSw0r0d",
-				[fullName.name]: "Mosh Hamedani",
-			},
-			errors: {},
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.componentsProps = {
+      username: {
+        name: "username",
+        label: "Username",
+      },
+      password: {
+        name: "password",
+        label: "Password",
+        type: "password",
+      },
+      fullName: {
+        name: "fullname",
+        label: "Full Name",
+      },
+    };
+    const { username, password, fullName } = this.componentsProps;
+    this.schema = {
+      [username.name]: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .required()
+        .label(username.label),
+      [password.name]: Joi.string()
+        .min(5)
+        .max(20)
+        .alphanum()
+        .required()
+        .label(password.label),
+      [fullName.name]: Joi.string().min(3).required().label(fullName.label),
+    };
+    this.state = {
+      data: {
+        [username.name]: "moshegg",
+        [password.name]: "myS3cretPasSw0r0d",
+        [fullName.name]: "Mosh Hamedani",
+      },
+      errors: {},
+    };
+  }
 
-	doSubmit = () => {
-		console.log("Register form submitted.");
-	};
+  doSubmit = () => {
+    console.log("Register form submitted.");
+  };
 
-	render() {
-		const { handleOnSubmit, componentsProps } = this;
-		const { username, password, fullName } = componentsProps;
-		return (
-			<form onSubmit={(e) => handleOnSubmit(e)}>
-				{this.renderInput(username)}
-				{this.renderInput(password)}
-				{this.renderInput(fullName)}
-				{this.renderSubmitButton("Register")}
-			</form>
-		);
-	}
+  render() {
+    const { handleOnSubmit, componentsProps } = this;
+    const { username, password, fullName } = componentsProps;
+    return (
+      <Form onSubmit={(e) => handleOnSubmit(e)}>
+        {this.renderInput(username)}
+        {this.renderInput(password)}
+        {this.renderInput(fullName)}
+        {this.renderSubmitButton("Register")}
+      </Form>
+    );
+  }
 }
 
 export default RegisterForm;
